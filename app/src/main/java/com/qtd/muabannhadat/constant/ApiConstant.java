1d6213b4f58a0c7da2f979aa8f2fbd936ec34f54
doannh <ngodoan.utc@gmail.com>
2016-04-01 22:51:47 +0700

    done check

---
+++ app/src/main/java/com/qtd/muabannhadat/constant/ApiConstant.java
@@ -0,0 +1,13 @@
+package com.qtd.muabannhadat.constant;
+
+/**
+ * Created by Ngo Hado on 01-Apr-16.
+ */
+public class ApiConstant {
+    public static final String NAME_SPACE = "http://tranhongquan.com/";
+    public static final String MAIN_URL = "http://nckhbds.somee.com/WebServiceNCKH.asmx?WSDL";
+
+    public static final String METHOD_LOGIN = "GetMemberinfobyIDnPassWord";
+    public static final String METHOD_REGISTER = "InsertMember";
+    public static final String METHOD_CHECK_USER = "ChecHaskUsername";
+}
