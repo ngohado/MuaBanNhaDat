1ca870c17432dcea1eea5bbe48dd98b70f6ebfa3
doannh <ngodoan.utc@gmail.com>
2016-04-11 00:45:13 +0700

    change UI

---
+++ app/src/main/java/com/qtd/muabannhadat/activity/ApartmentDetailActivity.java
@@ -0,0 +1,15 @@
+package com.qtd.muabannhadat.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.qtd.muabannhadat.R;
+
+public class ApartmentDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_apartment_detail);
+    }
+}
