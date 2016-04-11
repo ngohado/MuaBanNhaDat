cc6cc44b60d8ad2ef7f32b86af81a040eccaa2c4
doannh <ngodoan.utc@gmail.com>
2016-04-11 15:18:40 +0700

    change UI

--- app/src/main/java/com/qtd/muabannhadat/activity/ApartmentDetailActivity.java
+++ app/src/main/java/com/qtd/muabannhadat/activity/ApartmentDetailActivity.java
@@ -2,14 +2,23 @@ package com.qtd.muabannhadat.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.qtd.muabannhadat.R;
 
-public class ApartmentDetailActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
+public class ApartmentDetailActivity extends AppCompatActivity {
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_apartment_detail);
+        ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
     }
 }
