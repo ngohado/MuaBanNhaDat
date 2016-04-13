2255af9c96b5e23473beed3aa840ee1e9fa6b5d5
doannh <ngodoan.utc@gmail.com>
2016-04-14 00:47:31 +0700

    fix login

--- app/src/main/java/com/qtd/muabannhadat/activity/LoginActivity.java
+++ app/src/main/java/com/qtd/muabannhadat/activity/LoginActivity.java
@@ -39,11 +39,6 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
 
-
-        Intent intent = new Intent(this, ApartmentDetailActivity.class);
-        intent.putExtra(ApartmentDetailActivity.ID_APARTMENT, 1);
-        startActivity(intent);
-
         initView();
         btnLogin.setOnClickListener(this);
         btnRegister.setOnClickListener(this);
