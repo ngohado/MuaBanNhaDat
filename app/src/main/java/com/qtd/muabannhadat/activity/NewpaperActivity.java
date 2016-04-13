b6279d550beb646080ef22afc20aef3f2702de08
haitv <tranviethai2802@gmail.com>
2016-04-13 18:21:49 +0700

    Newspaper

--- app/src/main/java/com/qtd/muabannhadat/activity/NewpaperActivity.java
+++ app/src/main/java/com/qtd/muabannhadat/activity/NewpaperActivity.java
@@ -1,24 +1,46 @@
 package com.qtd.muabannhadat.activity;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.qtd.muabannhadat.R;
 import com.qtd.muabannhadat.database.MyDatabase;
 import com.qtd.muabannhadat.model.District;
 import com.qtd.muabannhadat.model.Street;
 
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class NewpaperActivity extends AppCompatActivity {
+public class NewpaperActivity extends AppCompatActivity implements View.OnClickListener {
     private Spinner spFrom;
     private Spinner spKind;
     private Spinner spCity;
@@ -26,11 +48,15 @@ public class NewpaperActivity extends AppCompatActivity {
     private Spinner spStreet;
     private EditText edtAcrea;
     private EditText edtCost;
-    private EditText edtMoTa;
-    private Button btnDangTin;
+    private EditText edtDescribe;
+    private EditText edtRoom;
+    private Button btnPost;
+    private Button btnSelect;
+    private ImageView ivImage;
     private MyDatabase myDatabase;
     private List<District> mDistrict;
     private List<Street> mStreet;
+    private int REQUEST_CAMERA = 0, SELECT_FILE = 1;
 
     private List<Street> streetsByDistrict = new ArrayList<>();
     private ArrayAdapter<District> districtAdapter;
@@ -46,6 +72,8 @@ public class NewpaperActivity extends AppCompatActivity {
         initData();
         spinnerFrom();
         spinnerCity();
+        btnPost.setOnClickListener(this);
+        btnSelect.setOnClickListener(this);
     }
 
     private void initData() {
@@ -89,8 +117,22 @@ public class NewpaperActivity extends AppCompatActivity {
         spStreet = (Spinner) findViewById(R.id.spStreet);
         edtAcrea = (EditText) findViewById(R.id.edtAcrea);
         edtCost = (EditText) findViewById(R.id.edtCost);
-        edtMoTa = (EditText) findViewById(R.id.edtMoTa);
-        btnDangTin = (Button) findViewById(R.id.btnDangTin);
+        edtDescribe = (EditText) findViewById(R.id.edtDescribe);
+        edtRoom = (EditText) findViewById(R.id.edtRoom);
+        btnSelect = (Button) findViewById(R.id.btnSelect);
+        ivImage = (ImageView) findViewById(R.id.ivImage);
+        btnPost = (Button) findViewById(R.id.btnPost);
+
+        edtDescribe.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    if (edtDescribe.length() > 3000) {
+                        edtDescribe.setError("Tối đa chỉ 3000 ký tự");
+                    }
+                }
+            }
+        });
     }
 
     private void spinnerCity() {
@@ -132,5 +174,171 @@ public class NewpaperActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    public Boolean validate() {
+        if (edtAcrea.getError() != null) return false;
+        if (edtRoom.getError() != null) return false;
+        if (edtCost.getError() != null) return false;
+        if (edtDescribe.getError() != null) return false;
+        return true;
+    }
+
+    private Boolean areEmpty() {
+        String dienTich = edtAcrea.getText().toString();
+        String tongTien = edtCost.getText().toString();
+        String phong = edtRoom.getText().toString();
+        if (dienTich.equals("") | tongTien.equals("") | phong.equals("")) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnPost:
+                if (!isNetworkAvailable()) {
+                    Toast.makeText(NewpaperActivity.this, "Hãy kiểm tra kết nối mạng!", Toast.LENGTH_SHORT).show();
+                } else {
+                    btnPostOnClick();
+                }
+                break;
+            case R.id.btnSelect:
+                selectImage();
+                break;
+        }
+
+    }
+
+    private void selectImage() {
+        final CharSequence[] items = {"Chụp ảnh", "Chọn từ thư mục",
+                "Thoát"};
+        AlertDialog.Builder builder = new AlertDialog.Builder(NewpaperActivity.this);
+        builder.setTitle("Chọn ảnh!");
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+                if (items[item].equals("Chụp ảnh")) {
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(intent, REQUEST_CAMERA);
+                } else if (items[item].equals("Chọn từ thư mục")) {
+                    Intent intent = new Intent(
+                            Intent.ACTION_PICK,
+                            android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    intent.setType("image/*");
+                    startActivityForResult(
+                            Intent.createChooser(intent, "Select File"),
+                            SELECT_FILE);
+                } else if (items[item].equals("Thoát")) {
+                    dialog.dismiss();
+                }
+            }
+        });
+        builder.show();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == NewpaperActivity.RESULT_OK) {
+            if (requestCode == SELECT_FILE)
+                onSelectFromGalleryResult(data);
+            else if (requestCode == REQUEST_CAMERA)
+                onCaptureImageResult(data);
+        }
+    }
+
+    private void onCaptureImageResult(Intent data) {
+        Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        thumbnail.compress(Bitmap.CompressFormat.JPEG, 90, bytes);
+
+        File destination = new File(Environment.getExternalStorageDirectory(),
+                System.currentTimeMillis() + ".jpg");
+
+        FileOutputStream fo;
+        try {
+            destination.createNewFile();
+            fo = new FileOutputStream(destination);
+            fo.write(bytes.toByteArray());
+            fo.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        ivImage.setImageBitmap(thumbnail);
+    }
+
+    @SuppressWarnings("deprecation")
+    private void onSelectFromGalleryResult(Intent data) {
+        Uri selectedImageUri = data.getData();
+        String[] projection = {MediaStore.MediaColumns.DATA};
+        Cursor cursor = managedQuery(selectedImageUri, projection, null, null, null);
+        int column_index = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
+        cursor.moveToFirst();
+
+        String selectedImagePath = cursor.getString(column_index);
+
+        Bitmap bm;
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(selectedImagePath, options);
+        final int REQUIRED_SIZE = 200;
+        int scale = 1;
+        while (options.outWidth / scale / 2 >= REQUIRED_SIZE
+                && options.outHeight / scale / 2 >= REQUIRED_SIZE)
+            scale *= 2;
+        options.inSampleSize = scale;
+        options.inJustDecodeBounds = false;
+        bm = BitmapFactory.decodeFile(selectedImagePath, options);
+
+        ivImage.setImageBitmap(bm);
+    }
+
+    private void btnPostOnClick() {
+        if (areEmpty() | !validate()) {
+            Toast.makeText(NewpaperActivity.this, "Hãy nhập đầy đủ và chính xác thông tin!", Toast.LENGTH_SHORT).show();
+        } else {
+
+        }
+
+    }
+
+    class PostAsyncTask extends AsyncTask<String,Void,String>{
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+        }
+    }
+
+    private String toJson(String[] params){
+        try{
+            JSONObject obj=new JSONObject();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
     }
 }
